1df4544180307bd83ff24874c690979b8b6cff9b
hogino <hogino1003@gmail.com>
2015-02-16 23:48:45 +0900

    create yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngYeomanSassTsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ngYeomanSassTsApp
+ */
+angular.module('ngYeomanSassTsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngYeomanSassTsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngYeomanSassTsApp
+ */
+angular.module('ngYeomanSassTsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
